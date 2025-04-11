6ae8f6cfdfe9bd70719944ddb916c2f296480cfb
MILIND SINGH <116150481+bit-milind42@users.noreply.github.com>
2025-04-11 21:02:55 +0530

    Update JwtConstant.java

--- src/main/java/com/milind/config/JwtConstant.java
+++ src/main/java/com/milind/config/JwtConstant.java
@@ -1,7 +1,7 @@
 package com.milind.config;
 
 public class JwtConstant {
-    public static final String SECRET_KEY="sxdcfwertyuikmnbvcxsertghnmkloiuygfcvgbhnjmk;,ijijcvbjhgf'.,gvhbjn";
+    public static final String SECRET_KEY="enter your secret key";
     public static final String JWT_HEADER="Authorization";
     
 }
